a3d436fd2c298da3c4d4834951a2eebe1f976393
Paul Deen <paul.deen@dutchworkz.nl>
2025-02-22 15:59:59 +0100

    Minor: Moved using to global usings

--- src/DatabaseFramework.Domain.Tests/GlobalUsings.cs
+++ src/DatabaseFramework.Domain.Tests/GlobalUsings.cs
@@ -1,7 +1,8 @@
-global using System.ComponentModel.DataAnnotations;
+﻿global using System.ComponentModel.DataAnnotations;
 global using CrossCutting.Common.Extensions;
 global using DatabaseFramework.Domain.Builders;
 global using DatabaseFramework.Domain.Builders.Extensions;
 global using DatabaseFramework.Domain.Builders.SqlStatements;
 global using DatabaseFramework.Domain.Domains;
 global using Shouldly;
+global using Xunit;
